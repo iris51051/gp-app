18c2aa6db1f54bae2c1b4d4e5e3b6ed980356add
iris51051 <iris5105@bispring.co.kr>
2023-08-07 15:54:45 +0900

    latest coomit

--- src/test/temp/api-test/DataFetchComponent.js
+++ src/test/temp/api-test/DataFetchComponent.js
@@ -3,35 +3,34 @@
 import axios from 'axios';
 
 const fetchData = async () => {
+  const data = JSON.stringify({
+    rptNo: '1000000',
+    lookupTp: 'agg',
+    dimCd: ['by_day', 'keyword', 'campaign'],
+    metCd: ['land', 'm_impr', 'm_click'],
+    where: [
+      {
+        field: 'stat_date',
+        operation: 'between',
+        value: ['2023-07-10', '2023-07-31'],
+      },
+    ],
+    sort: [{ field: 'land', order: 'asc' }],
+    agencySeq: '1',
+    clientSeq: '106702',
+    pfno: ['300004'],
+    size: 30,
+})
+const header = {
+  headers: { 'Content-Type': 'application/json', 'X-Authorization-User': 'blues'}
+ 
+}
   try {
     const response = await axios.post(
-      'http://122.99.192.199:8120/report/data',
-      {
-        rptNo: '1000000',
-        lookupTp: 'agg',
-        dimCd: ['by_day', 'keyword', 'campaign'],
-        metCd: ['land', 'm_impr', 'm_click'],
-        where: [
-          {
-            field: 'stat_date',
-            operation: 'between',
-            value: ['2023-07-10', '2023-07-31'],
-          },
-        ],
-        sort: [{ field: 'land', order: 'asc' }],
-        agencySeq: '1',
-        clientSeq: '106702',
-        pfno: ['300004'],
-        size: 30,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'x-authorization-user': 'blues',
-        },
-      }
+      'http://122.99.192.144:9080/report/data',
+      data,
+      header
     );
-
     return response.data;
   } catch (error) {
     console.error('Error fetching data:', error);
@@ -40,3 +39,93 @@ const fetchData = async () => {
 };
 
 export default fetchData;
+
+// const fetchData = async () => {
+//   try {
+//     const response = await fetch('http://122.99.192.144:9080/report/data', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'x-authorization-user': 'blues',
+//       },
+//       body: JSON.stringify({
+//         rptNo: '1000000',
+//         lookupTp: 'agg',
+//         dimCd: ['by_day', 'keyword', 'campaign'],
+//         metCd: ['land', 'm_impr', 'm_click'],
+//         where: [
+//           {
+//             field: 'stat_date',
+//             operation: 'between',
+//             value: ['2023-07-10', '2023-07-31'],
+//           },
+//         ],
+//         sort: [{ field: 'land', order: 'asc' }],
+//         agencySeq: '1',
+//         clientSeq: '106702',
+//         pfno: ['300004'],
+//         size: 30,
+//       }),
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     return null;
+//   }
+// };
+
+// export default fetchData;
+
+
+// fetch
+
+// const fetchData = async () =>{
+//   const url = 'http://122.99.192.199:8120/report/data';
+
+//   const headers = {
+//     'Content-Type': 'application/json',
+//     'x-authorization-user': 'blues',
+//   };
+
+//   const data = {
+//     rptNo: '1000000',
+//     lookupTp: 'agg',
+//     agencySeq: '1',
+//     clientSeq: '106702',
+//     pfno: ['300004'],
+//   };
+
+//   const requestOptions = {
+//     method: 'POST',
+//     headers: headers,
+//     body: JSON.stringify(data),
+//   };
+
+//   try {
+//     const response = await fetch(url, requestOptions);
+//     const jsonData = await response.json();
+//     console.log(jsonData);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// }
+
+// export default fetchData
+
+
+// const YourComponent = () => {
+//   // You can trigger the fetch function using a button click or other events
+//   const handleFetchData = () => {
+//     fetchData();
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handleFetchData}>Fetch Data</button>
+//     </div>
+//   );
+// };
+
+// export default YourComponent;
---
+++ src/test/temp/api-test/test.js
@@ -0,0 +1,46 @@
+// App.js
+
+import React, { useEffect,useState } from 'react';
+import axios from 'axios';
+import fetchData from './DataFetchComponent'; // Replace with the correct path to DataFetchComponent.js
+
+const Apitest = () => {
+  const [responseData, setResponseData] = useState(null);
+
+  const handleApiCall = async () => {
+    try {
+      // Use the JSONPlaceholder API '/posts' endpoint for POST request
+      const response = await axios.post('http://122.99.192.144:9080/report/data', {
+        rptNo: '1000000',
+        lookupTp: 'agg',
+        agencySeq: '1',
+        clientSeq: '106702',
+        pfno: ['300004'],
+      },{
+        headers:{
+          'x-authorization-user': 'blues'
+        }
+      });
+
+      // Handle the API response data here
+      setResponseData(response.data);
+    } catch (error) {
+      // Handle any errors that occurred during the API call
+      console.error('Error making API call:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleApiCall}>Make API Call</button>
+      {responseData && (
+        <div>
+          <h2>API Response:</h2>
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Apitest;
