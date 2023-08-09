e96d827fd0af43b2f8d63247d2581b2325cd12f0
iris51051 <iris5105@bispring.co.kr>
2023-08-09 17:31:26 +0900

    불필요한 ad 삭제

--- src/test/data/AdData.js
+++ src/test/data/AdData.js
@@ -4,15 +4,6 @@ export const AdData =[
     {value:"105580", name: '모바일미샤'},
     {value:"106658", name: '비즈스프링_대행사'},
     {value:"106659", name: 'A 비즈스프링'},
-    {value:"106666", name: '옥경화_네스프레소_광고주 생성'},
-    {value:"106668", name: '옥경화_라이프하우스'},
-    {value:"106683", name: '진실 테스트 220726'},
-    {value:"106776", name: '국가대표광고'},
-    {value:"106788", name: '재홍 테스트 230103'},
-    {value:"106794", name: 'oniontest'},
-    {value:"106806", name: '욱진_테스트'},
-    {value:"106816", name: '(주)교육지대'},
-    {value:"106817", name: '미샤 에이블씨엔씨'},
-    {value:"106839", name: '안성재_페이스북'},
+
 ]
 export default AdData;
