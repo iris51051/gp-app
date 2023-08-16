e1649c8c77ade794cff411ab04f6cea55f2231e1
iris51051 <iris5105@bispring.co.kr>
2023-08-16 14:46:00 +0900

    Login api요청

--- src/test/temp/api-test/Apitest.js
+++ src/test/temp/api-test/Apitest.js
@@ -2,9 +2,11 @@
 
 import React, { useEffect,useState } from 'react';
 import fetchData from './DataFetchComponent'; // Replace with the correct path to DataFetchComponent.js
+import authData from './urltest'; // Replace with the correct path to DataFetchComponent.js
 
 const Apitest = () => {
   const [testData, setTestData] = useState(null)
+  const [testToken, setTestToken] = useState(null)
   useEffect(() => {
     const getDataAndLog = async () => {
       const data = await fetchData();
@@ -14,11 +16,21 @@ const Apitest = () => {
     };
     getDataAndLog();
   }, []);
+  useEffect(() => {
+    const getAythToken = async () => {
+      const data = await authData();
+      if (data && data.length >0) {
+        setTestToken(data[0]);
+      }
+    };
+    getAythToken();
+  }, []);
 
   return (
     <div>
       <h1>api 테스트 페이지</h1>
       {testData && <p>테스트 데이터 : {testData.ad_program}</p>}
+      {testToken && <p>테스트 토큰 : {testToken}</p>}
       <br/>
       <br/>
       <br/>
---
+++ src/test/temp/api-test/urltest.js
@@ -0,0 +1,25 @@
+// DataFetchComponent.js
+
+import axios from 'axios';
+
+const authData = async () => {
+
+  try {
+    const response = await axios.post(
+        'http://223.130.136.182/auth/signin',
+        {
+          username: 'blues',
+          password: 'Tmakxm123$'
+        },
+        {
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
+    return response.data.token;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+};
+
+export default authData;
